47bbdcd29622b58cb3413d8ca7f673a76c265e25
fachryadhitya <fachryadhitya@gmail.com>
2024-09-18 22:56:54 +0700

    feat: add user entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,26 @@
+import { ObjectType, Field, Int, registerEnumType } from '@nestjs/graphql';
+import { Task } from '../../tasks/entities/task.entity';
+
+export enum Role {
+    USER = 'USER',
+    ADMIN = 'ADMIN',
+}
+
+registerEnumType(Role, {
+    name: 'Role',
+});
+
+@ObjectType()
+export class User {
+    @Field(() => Int)
+    id: number;
+
+    @Field()
+    email: string;
+
+    @Field(() => Role)
+    role: Role;
+
+    @Field(() => [Task])
+    tasks: Task[];
+}
